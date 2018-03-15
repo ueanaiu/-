045ae1187322b0f1f72ec5c04280d46ccf978365
ueanaiu <35555343+ueanaiu@users.noreply.github.com>
2018-03-15 14:53:00 +0800

    Create Dom.js

---
+++ Dom.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>DOM</title>
+	</head>
+	<body>
+		<!--<div></div>-->
+		<a href="">超链接</a>
+		<p>测试文字</p>
+		<img src="" alt="" />
+		<!--
+			a,p,body,html...元素节点
+			href src alt  属性节点
+			"测试文字"  文本节点
+			document   document节点
+			<!--<div></div>- 注释节点 -> 
+		-->
+	</body>
+	<script>
+		/*
+		 * DOM -> Document Object Model(文档对象模型)
+		 * 是HTML和XML文档的编程接口，提供了对文档的结构化的描述
+		 * 
+		 * dom节点是DOM中最基本的组成单元
+		 * 按照层级关系划分：父节点、子节点、兄弟节点
+		 * 按照类型划分：1.元素节点; 2.属性节点; 3.文本节点; 8.注释节点; 9.document节点
+		 * 
+		*/
+		
+		/*
+		 * 	children：子节点       非标准属性
+		 * 	不会把换行解析成空白文本节点
+		 * 
+		 *  childNodes:子节点    标准属性
+		 * 会把换行解析成空白文本空节点
+		 * 	
+		*/
+		
+	</script>
+</html>
